8267cefeb427eb800de822d8d0043d56a9081426
SaiVarun <59646676+varun2014@users.noreply.github.com>
2021-05-24 16:52:40 +0530

    Add files via upload

---
+++ varun program1/varun_  program1/script.js
@@ -0,0 +1,30 @@
+function clearOutput(){
+	document.getElementById('output').innerHTML="0";
+}
+
+function perc(){
+	var value = document.getElementById("output").innerHTML;
+	value = value/100
+	document.getElementById("output").innerHTML=value;
+}
+
+function removeZero() {
+      var value = document.getElementById("output").innerHTML;
+      if (value == "0") {
+           value = " "
+           document.getElementById("output").innerHTML = value;
+      }
+}
+
+function fordisplay(value) {
+      removeZero()
+      document.getElementById("output").innerHTML += value;
+}
+
+function solve(){
+	removeZero()
+	var equation = document.getElementById("output").innerHTML;
+	var solved = eval(equation);
+	document.getElementById("output").innerHTML=solved;
+
+}
